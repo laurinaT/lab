be72c46c8c601c5d7a8a302b36839c31d9fdc096
Laurina Toussaint <laurinatoussaint@gmail.com>
2021-09-16 16:17:36 +0200

    travail du jour foncions 2

---
+++ atelier_js/js/fonctions.js
@@ -0,0 +1,14 @@
+function greetings(firstname, lastname){
+    return "Bienvenue " + firstname + " " + lastname;
+}
+
+// var greetings = function(){}
+//     console.log("bienvenue");
+
+// ou (mais en haut est plus standard)
+// Appel de la fonction pour éxécution
+ greetings("Thomas", "Gossart");
+ greetings("Tony", "Micceli");
+
+var speach = greetings("Gégé", "Depardieu");
+console.log(speach);
---
+++ exercice_js/js/fonctions.js
@@ -0,0 +1,38 @@
+// Exercice 1
+
+Math.random();
+var retour = Math.random();
+console.log(retour);
+
+
+// Exercice 2
+
+var number = 12.42359;
+var num = number.toFixed(2);
+console.log(num);
+
+// Exercice 3
+
+var age = "35";
+console.log(typeof age);
+parseInt(age);
+console.log(age);
+
+// Exercice 4
+
+var text = "Ceci est un texte";
+var maxLength = 20;
+console.log(text.length);
+
+var texte = "Ceci eest un trèèèèès grand teeeeeext";
+if (texte.length > 20) {
+    console.log("Your text is too long");
+}
+else {
+    console.log("OK");
+}
+
+//Exercice 5
+
+var text = "A nice boat";
+console.log(typeof text);
